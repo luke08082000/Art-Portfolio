41c45fe1614d74cbb5a42f929801718396c41a5c
Luke <luke.manongsong.cics@ust.edu.ph>
2022-05-21 14:07:58 +0800

    new font

--- code.js
+++ code.js
@@ -14,9 +14,7 @@ images.forEach(image => {
         lightbox.appendChild(img);
     })
 })
-
 lightbox.addEventListener('click', e => {
     if (e.target !== e.currentTarget) return;
         lightbox.classList.remove('active');
-    
 })
