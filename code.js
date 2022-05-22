9c677e23c393b5084a7f47a905547808d4f420a0
Luke <luke.manongsong.cics@ust.edu.ph>
2022-05-22 17:06:04 +0800

    added slider

--- code.js
+++ code.js
@@ -1,20 +1,43 @@
-const lightbox = document.createElement('div');
-lightbox.id = 'lightbox';
-document.body.appendChild(lightbox);
+const lightbox = document.querySelector('#lightbox');
+const closeBtn = document.querySelector('#close');
+const prev = document.querySelector('#prev');
+const next = document.querySelector('#next');
+const buttons = document.querySelector('#buttons');
 
 const images = document.querySelectorAll('img');
+const slide = Array.from(images);
+
 images.forEach(image => {
     image.addEventListener('click', e => {
         lightbox.classList.add('active');
+        buttons.style.display = 'block';
         const img = document.createElement('img');
         img.src = image.src;
         while (lightbox.firstChild) {
             lightbox.removeChild(lightbox.firstChild);
         }
         lightbox.appendChild(img);
+        console.log(slide.indexOf(image))
+        let index = slide.indexOf(image)
+        prev.addEventListener('click', e => {
+            index--;
+            if (index < 0) {
+                index = slide.length-1;
+                img.src = slide[index].src;
+            }
+            img.src = slide[index].src;
+        })
+        next.addEventListener('click', e => {
+            index++;
+            if (index > slide.length-1) {
+                index = 0;
+                img.src = slide[index].src;
+            }
+            img.src = slide[index].src;
+        })
     })
 })
-lightbox.addEventListener('click', e => {
-    if (e.target !== e.currentTarget) return;
-        lightbox.classList.remove('active');
+closeBtn.addEventListener('click', e => {
+    lightbox.classList.remove('active');
+    buttons.style.display = 'none';
 })
